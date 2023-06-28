3d9e7c429b7d8fa1bd580b60715803aa54c5b260
JordanBalabanski <jordanbalabanski@gmail.com>
2023-06-28 11:02:22 +0300

    view changes

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import random
 
+def computer_number():
+    return random.randint(1,15)
+
 def guess(x):
-    random_number = random.randint(1,x)
+    comp_num = computer_number()
+    if x==comp_num:
+        return 'You guessed it! Bravo!'
+    else:
+        return 'Try again!'
--- view.py
+++ view.py
@@ -1,19 +1,24 @@
 from tkinter import *
+from main import *
 
 tk = Tk()
 
-canvas = Canvas(tk, width=500, height=500)
+canvas = Canvas(tk, width=200, height=10)
 canvas.pack()
-
-
-def guess():
-    print("smth")
-
 
 label = Label(text="Try to guess the number!")
 label.pack()
 
 input_field = Entry()
+input_field.pack()
 
-check_btn = Button(tk, text="Guess", command=guess)
+
+def answer_check(x):
+    label = Label(text=guess(x))
+    label.pack()
+
+
+check_btn = Button(tk, text="Guess", command=lambda: answer_check(input_field.get()))
 check_btn.pack()
+
+tk.mainloop()
